db61b376bf2c2d84d055e2cbc4f2d8a0a929ac49
Ian Westcott <iwestcott@mbta.com>
2021-05-13 18:00:49 -0400

    feat: support template task definitions for Terraform-managed ECS services

--- deploy-ecs/deploy.sh
+++ deploy-ecs/deploy.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
 set -e -u
 
-# get JSON describing task definition currently running on AWS
-# use it as basis for new revision, but replace image with the one built above
-taskdefinition=$(aws ecs describe-task-definition --region us-east-1 --task-definition $ECS_SERVICE)
-taskdefinition=$(echo $taskdefinition | jq ".taskDefinition | del(.status) | del(.taskDefinitionArn) | del(.requiresAttributes) | del(.revision) | del(.compatibilities) | del(.registeredAt) | del(.registeredBy)")
-newcontainers=$(echo $taskdefinition | jq ".containerDefinitions | map(.image=\"$DOCKER_TAG\")")
-aws ecs register-task-definition --region us-east-1 --family $ECS_SERVICE --cli-input-json "$taskdefinition" --container-definitions "$newcontainers"
-newrevision=$(aws ecs describe-task-definition --region $AWS_REGION --task-definition $ECS_SERVICE | jq '.taskDefinition.revision')
+# attempt to get the contents of the template task definition from ECS (for Terraform-built ECS services)
+echo "Retrieving ${ECS_SERVICE}-template task definition..."
+taskdefinition="$(aws ecs describe-task-definition --region "${AWS_REGION}" --task-definition "${ECS_SERVICE}-template")" \
+  || echo "No template task definition was found."
+
+# if no template exists, attempt to get task definition currently running on AWS (for legacy ECS services)
+if [ -z "${taskdefinition}" ]; then
+  echo "Retreiving current ${ECS_SERVICE} task definition..."
+  taskdefinition=$(aws ecs describe-task-definition --region "${AWS_REGION}" --task-definition "${ECS_SERVICE}")
+fi
+
+# use retrieved task definition as basis for new revision, but replace image
+echo "Updating container image to ${DOCKER_TAG}."
+newcontainers="$(echo "${taskdefinition}" | \
+  jq '.taskDefinition.containerDefinitions' | \
+  jq --arg tag "${DOCKER_TAG}" 'map(.image="\($tag)")')"
+
+# check to make sure the secrets are included in the new container definition
+if (echo "${newcontainers}" | jq '.[0] | .secrets' | grep '^null$'); then
+  echo "Error: The container definition is missing its 'secrets' block. Deploy cannot proceed."
+  exit 1
+fi
+
+echo "Publishing new task definition."
+aws ecs register-task-definition \
+  --family "${ECS_SERVICE}" \
+  --task-role-arn "$(echo "${taskdefinition}" | jq -r '.taskDefinition.taskRoleArn')" \
+  --execution-role-arn "$(echo "${taskdefinition}" | jq -r '.taskDefinition.executionRoleArn')" \
+  --network-mode "$(echo "${taskdefinition}" | jq -r '.taskDefinition.networkMode')" \
+  --container-definitions "${newcontainers}" \
+  --volumes "$(echo "${taskdefinition}" | jq '.taskDefinition.volumes')" \
+  --placement-constraints "$(echo "${taskdefinition}" | jq '.taskDefinition.placementConstraints')" \
+  --requires-compatibilities "$(echo "${taskdefinition}" | jq -r '.taskDefinition.requiresCompatibilities[]')" \
+  --cpu "$(echo "${taskdefinition}" | jq -r '.taskDefinition.cpu')" \
+  --memory "$(echo "${taskdefinition}" | jq -r '.taskDefinition.memory')"
+
+newrevision="$(aws ecs describe-task-definition --task-definition "${ECS_SERVICE}" | \
+  jq -r '.taskDefinition.revision')"
 
 function task_count_eq {
     local tasks
