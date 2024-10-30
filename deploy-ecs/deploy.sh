5ae28d8a8519a3c671426672939b8e4787af6260
Ryan Rymarczyk <rymarczy@gmail.com>
2024-10-30 10:24:30 -0400

    shared function

--- deploy-ecs/deploy.sh
+++ deploy-ecs/deploy.sh
@@ -15,6 +15,9 @@ set -e -u
 # - ECS_TASK_DEF
 # - DOCKER_TAG
 
+# makes 'build_register_task_options' function available, produces 'reg_options' global var
+. ../shared/register_task_options.sh
+
 function check_deployment_complete() {
   # extract task counts and test whether they match the desired state
   local deployment_details
@@ -60,33 +63,6 @@ function check_deployment_complete() {
   [[ "${rollout_status}" = "COMPLETED" ]] \
   && [[ "${pending_count}" -eq "0" ]] \
   && [[ "${running_count}" -eq "${desired_count}" ]]
-}
-
-function build_register_task_options() {
-  # create array of command options for register-task-definition command (on FARGATE)
-  # global reg_options variable to be used as output
-  local task_def
-  local container_def
-
-  task_def="${1}"
-  container_def="${2}"
-
-  # build register-task-definition command options in array
-  reg_options=( --family "${ECS_TASK_DEF}" )
-  reg_options+=( --task-role-arn "$(echo "${task_def}" | jq -r '.taskDefinition.taskRoleArn')" )
-  reg_options+=( --execution-role-arn "$(echo "${task_def}" | jq -r '.taskDefinition.executionRoleArn')" )
-  reg_options+=( --network-mode "$(echo "${task_def}" | jq -r '.taskDefinition.networkMode')" )
-  reg_options+=( --container-definitions "${container_def}" )
-  reg_options+=( --volumes "$(echo "${task_def}" | jq '.taskDefinition.volumes')" )
-  reg_options+=( --placement-constraints "$(echo "${task_def}" | jq '.taskDefinition.placementConstraints')" )
-  reg_options+=( --requires-compatibilities "$(echo "${task_def}" | jq -r '.taskDefinition.requiresCompatibilities')" )
-  reg_options+=( --cpu "$(echo "${task_def}" | jq -r '.taskDefinition.cpu')" )
-  reg_options+=( --memory "$(echo "${task_def}" | jq -r '.taskDefinition.memory')" )
-  # include --ephemeral-storage option if available in template task definition
-  template_ephemeral_storage="$(echo "${task_def}" | jq -r '.taskDefinition.ephemeralStorage')"
-  if [ "$template_ephemeral_storage" != null ]; then
-      reg_options+=( --ephemeral-storage "${template_ephemeral_storage}")
-  fi
 }
 
 # set default region so we don't have to specify --region everywhere
--- deploy-scheduled-ecs/deploy.sh
+++ deploy-scheduled-ecs/deploy.sh
@@ -17,32 +17,8 @@ set -e -u
 # - ECS_TASK_DEF: task definition to update revision for
 # - DOCKER_TAG: tag for docker image to use in new task definition
 
-function build_register_task_options() {
-  # create array of command options for register-task-definition command (on FARGATE)
-  # global reg_options variable to be used as output
-  local task_def
-  local container_def
-
-  task_def="${1}"
-  container_def="${2}"
-
-  # build register-task-definition command options in array
-  reg_options=( --family "${ECS_TASK_DEF}" )
-  reg_options+=( --task-role-arn "$(echo "${task_def}" | jq -r '.taskDefinition.taskRoleArn')" )
-  reg_options+=( --execution-role-arn "$(echo "${task_def}" | jq -r '.taskDefinition.executionRoleArn')" )
-  reg_options+=( --network-mode "$(echo "${task_def}" | jq -r '.taskDefinition.networkMode')" )
-  reg_options+=( --container-definitions "${container_def}" )
-  reg_options+=( --volumes "$(echo "${task_def}" | jq '.taskDefinition.volumes')" )
-  reg_options+=( --placement-constraints "$(echo "${task_def}" | jq '.taskDefinition.placementConstraints')" )
-  reg_options+=( --requires-compatibilities "$(echo "${task_def}" | jq -r '.taskDefinition.requiresCompatibilities')" )
-  reg_options+=( --cpu "$(echo "${task_def}" | jq -r '.taskDefinition.cpu')" )
-  reg_options+=( --memory "$(echo "${task_def}" | jq -r '.taskDefinition.memory')" )
-  # include --ephemeral-storage option if available in template task definition
-  template_ephemeral_storage="$(echo "${task_def}" | jq -r '.taskDefinition.ephemeralStorage')"
-  if [ "$template_ephemeral_storage" != null ]; then
-      reg_options+=( --ephemeral-storage "${template_ephemeral_storage}")
-  fi
-}
+# makes 'build_register_task_options' function available, produces 'reg_options' global var
+. ../shared/register_task_options.sh
 
 # set default region so we don't have to specify --region everywhere
 export AWS_DEFAULT_REGION="${AWS_REGION}"
